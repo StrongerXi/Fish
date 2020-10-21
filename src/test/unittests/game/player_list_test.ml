f81d094c59dcade4d340827699f2e894b9f438cc
Ryan <ryan.guo99@gmail.com>
2020-10-20 22:09:22 -0400

    Finish player_list implementation

--- src/test/unittests/game/player_list_test.ml
+++ src/test/unittests/game/player_list_test.ml
@@ -22,7 +22,8 @@ let tests = OUnit2.(>:::) "player_list_tests" [
 
     OUnit2.(>::) "test_place_penguin" (fun _ ->
         (* 1. penguin is placed to the right player
-         * 2. no side effect *)
+         * 2. no side effect
+         * 3. errors on bad input *)
         let colors = [PC.White; PC.Brown; PC.Red] in
         let pl = PL.create colors in
         let pos11 = { Pos.row = 1; col = 1 } in
@@ -35,17 +36,22 @@ let tests = OUnit2.(>:::) "player_list_tests" [
         in
         OUnit2.assert_equal new_players (PL.get_ordered_players pl1);
 
-        let new_players = map_player_with_color players PC.Red
+        let new_players = map_player_with_color new_players PC.Red
             (fun p -> P.add_penguin p @@ PN.create pos23)
         in
         let pl2 = PL.place_penguin pl1 PC.Red pos23 in
         OUnit2.assert_equal new_players (PL.get_ordered_players pl2);
+
+        let expect = Failure "No player has given color" in
+        OUnit2.assert_raises
+          expect (fun () -> PL.place_penguin pl2 PC.Black pos11);
       );
 
     OUnit2.(>::) "test_move_penguin" (fun _ ->
         (* 1. penguin is moved
          * 2. player score is updated
-         * 3. no side effect *)
+         * 3. no side effect
+         * 4. errors on bad input *)
         let colors = [PC.Black; PC.White; PC.Red] in
         let pos10 = { Pos.row = 1; col = 0 } in
         let pos12 = { Pos.row = 1; col = 2 } in
@@ -70,7 +76,7 @@ let tests = OUnit2.(>:::) "player_list_tests" [
         in
         OUnit2.assert_equal new_players (PL.get_ordered_players new_pl);
         (* make another move, score should be accumulated *)
-        let new_pl = PL.move_penguin pl pos12 pos10 4 in
+        let new_pl = PL.move_penguin new_pl pos12 pos10 4 in
         let new_players = map_player_with_color players PC.White
             (fun _ ->
                let peng = PN.create pos10 in
@@ -78,6 +84,9 @@ let tests = OUnit2.(>:::) "player_list_tests" [
                P.set_score p 7)
         in
         OUnit2.assert_equal new_players (PL.get_ordered_players new_pl);
+        (* no penguin at source position *)
+        let expect = Failure "No penguin resides at source position" in
+        OUnit2.assert_raises expect (fun () -> PL.move_penguin new_pl pos12 pos10 4);
       );
   ]
 
