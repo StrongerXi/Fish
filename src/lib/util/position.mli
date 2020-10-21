f91728f4dd1ba1fa284f958afc222b031a3f3ba9
Ryan <ryan.guo99@gmail.com>
2020-10-20 20:25:25 -0400

    Add unit tests for position util module

--- src/lib/util/position.mli
+++ src/lib/util/position.mli
@@ -8,3 +8,7 @@ type t =
 (** Creates a list of distinct positions (row, col) for
     0 <= row < [height] and 0 <= column < [width] *)
 val create_positions_within : height:int -> width:int -> t list
+
+(** Compare 2 positions lexicalgraphically.
+    ex: (0, 0), (1, 2) -> -1 *)
+val compare : t -> t -> int
---
+++ src/test/unittests/util/position_test.ml
@@ -0,0 +1,34 @@
+module P = Fish.Util.Position
+
+let tests = OUnit2.(>:::) "position_tests" [
+
+    OUnit2.(>::) "test_compare" (fun _ ->
+        let p11 = { P.row = 1; col = 1 } in
+        let p03 = { P.row = 0; col = 3 } in
+        let p20 = { P.row = 2; col = 0 } in
+        let p33 = { P.row = 3; col = 3 } in
+        OUnit2.assert_equal true @@ ((P.compare p11 p03) < 0);
+        OUnit2.assert_equal true @@ ((P.compare p11 p20) > 0);
+        OUnit2.assert_equal true @@ ((P.compare p20 p33) < 0);
+        OUnit2.assert_equal true @@ ((P.compare p33 p33) = 0);
+      );
+
+    OUnit2.(>::) "test_create_positions" (fun _ ->
+        let positions = P.create_positions_within ~height:2 ~width:3
+                        |> List.sort P.compare
+        in
+        let expect = [ { P.row = 0; col = 0 };
+                       { P.row = 0; col = 1 };
+                       { P.row = 0; col = 2 };
+                       { P.row = 1; col = 0 };
+                       { P.row = 1; col = 1 };
+                       { P.row = 1; col = 2 };
+                     ]
+                     |> List.sort P.compare
+        in
+        OUnit2.assert_equal expect positions
+      );
+  ]
+
+let _ =
+  OUnit2.run_test_tt_main tests
