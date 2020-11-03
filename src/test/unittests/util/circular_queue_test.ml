ff2a191f45768534d8de184e01d6b3daddc321a1
Ryan <ryan.guo99@gmail.com>
2020-11-02 22:14:57 -0500

    Add unit tests for the circular queue utility module

---
+++ src/test/unittests/util/circular_queue_test.ml
@@ -0,0 +1,37 @@
+module CQ = Fish.Util.Circular_queue
+
+let tests = OUnit2.(>:::) "circular_queue_tests" [
+
+    OUnit2.(>::) "test_create" (fun _ ->
+        let int_cq = CQ.create 42 [] in
+        OUnit.assert_equal 42 @@ CQ.get_current int_cq;
+        OUnit.assert_equal [42;] @@ CQ.to_list int_cq;
+
+        let int_cq = CQ.create 2 [1; 3;] in
+        OUnit.assert_equal 2 @@ CQ.get_current int_cq;
+        OUnit.assert_equal [2; 1; 3;] @@ CQ.to_list int_cq;
+      );
+
+    OUnit2.(>::) "test_rotate" (fun _ ->
+        let int_cq = CQ.create 17 [] |> CQ.rotate in
+        OUnit.assert_equal 17 @@ CQ.get_current int_cq;
+        OUnit.assert_equal [17;] @@ CQ.to_list int_cq;
+
+        let int_cq = CQ.create 2 [1; 3;] in
+
+        let int_cq = CQ.rotate int_cq in
+        OUnit.assert_equal [1; 3; 2;] @@ CQ.to_list int_cq;
+        OUnit.assert_equal 1 @@ CQ.get_current int_cq;
+
+        let int_cq = CQ.rotate int_cq in
+        OUnit.assert_equal [3; 2; 1;] @@ CQ.to_list int_cq;
+        OUnit.assert_equal 3 @@ CQ.get_current int_cq;
+
+        let int_cq = CQ.rotate int_cq in
+        OUnit.assert_equal [2; 1; 3;] @@ CQ.to_list int_cq;
+        OUnit.assert_equal 2 @@ CQ.get_current int_cq;
+      );
+  ]
+
+let _ =
+  OUnit2.run_test_tt_main tests
