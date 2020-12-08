c17957d0ab82c61cd2d313e687c0e38cb5f83504
Ryan <ryan.guo99@gmail.com>
2020-12-08 00:02:43 -0500

    Implement timeout using signal handler

--- src/lib/admin/timeout_util.ml
+++ src/lib/admin/timeout_util.ml
@@ -1,6 +1,16 @@
 open !Core
+module Sys = Stdlib.Sys
 
-let call_with_timeout_ms (f : unit -> 'a) (_ : int) : 'a option =
-  (* TODO can't find a good solution. Tried Async, Lwt and Thread *)
-  Option.some @@ f ()
-;;
+(* TODO This probably can't be used by multiple threads concurrently *)
+exception Timeout
+let call_with_timeout_ms (thunk : unit -> 'a) (ms : int) : 'a option =
+  Core.ignore @@ (* time out once after [ms] ms *)
+  Unix.setitimer Unix.ITIMER_REAL { Unix.it_interval = 0.0;
+                                    it_value = (float_of_int ms) };
+  Sys.set_signal Sys.sigalrm (Sys.Signal_handle (fun _ -> raise Timeout));
+  try 
+    let res = Some(thunk()) in
+    Core.ignore @@ (* cancel timeout since the computation is finished *)
+    Unix.setitimer Unix.ITIMER_REAL { Unix.it_interval = 0.0; it_value = 0.0};
+    res
+  with Timeout -> None
