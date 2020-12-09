dadac50f0e8c028d49d31e41f096fe121b15fe40
Ryan <ryan.guo99@gmail.com>
2020-12-09 11:22:59 -0500

    [signup_manager] Refactor and fix player count checking bug. Always shut down server socket

--- src/lib/remote/signup_manager.ml
+++ src/lib/remote/signup_manager.ml
@@ -23,11 +23,11 @@ let create_server (conf : config) (port : int) : Unix.File_descr.t =
 ;;
 
 let handle_waiting_period (server : Unix.File_descr.t)
-    (conf : config) (players : Player.t list) : Player.t list =
+    (conf : config) (connected_players : Player.t list) : Player.t list =
   let buf = Core.Bytes.create conf.max_name_bytes in
   let rec loop
       (players : Player.t list) (time_left_ms : int) : Player.t list =
-    if (List.length players) > conf.max_num_of_players
+    if (List.length players) >= conf.max_num_of_players
     then players
     else
       let start_ms = Unix.gettimeofday () in
@@ -35,22 +35,26 @@ let handle_waiting_period (server : Unix.File_descr.t)
               (fun () -> Unix.accept server) time_left_ms with
       | None -> players (* this waiting period is over *)
       | Some(client_sock, _) ->
-        let opt_name = Timeout.call_with_timeout_ms (fun () ->
-            Core.ignore @@ Unix.read ~pos:0 ~len:conf.max_name_bytes ~buf client_sock;
-            Core.Bytes.to_string buf) conf.name_reply_timeout_ms in
+        let players = try_to_add_client players client_sock in
         let time_passed_ms =
-          int_of_float Float.(1000. * (Unix.gettimeofday()) - start_ms) in
-        let time_left_ms = time_left_ms - time_passed_ms in
-        match opt_name with
-        | None -> loop players time_left_ms
-        | Some(name) -> 
-          let ic = Unix.in_channel_of_descr client_sock in
-          let oc = Unix.out_channel_of_descr client_sock in
-          let age = List.length players in
-          let p = Remote_player.create_proxy_player ic oc name age in
-          loop (p::players) time_left_ms
+          int_of_float Float.(1000. * ((Unix.gettimeofday()) - start_ms)) in
+        loop players (time_left_ms - time_passed_ms)
+  (* Add [client_sock] as a proxy player to [players] if it sends a name in time *)
+  and try_to_add_client (players : Player.t list) (client_sock : Unix.File_descr.t)
+    : Player.t list =
+    let opt_name = Timeout.call_with_timeout_ms (fun () ->
+        let n = Unix.read ~pos:0 ~len:conf.max_name_bytes ~buf client_sock in
+        Core.Bytes.sub buf ~pos:0 ~len:n |> Core.Bytes.to_string)
+        conf.name_reply_timeout_ms in
+    match opt_name with
+    | None -> players
+    | Some(name) -> 
+      let ic = Unix.in_channel_of_descr client_sock in
+      let oc = Unix.out_channel_of_descr client_sock in
+      let age = List.length players in
+      (Remote_player.create_proxy_player ic oc name age)::players
   in
-  loop players conf.waiting_period_ms
+  loop connected_players conf.waiting_period_ms
 ;;
 
 let sign_up conf port =
@@ -63,5 +67,10 @@ let sign_up conf port =
       else loop server (num_of_wp_left - 1) players
   in
   let server = create_server conf port in
-  loop server conf.num_of_waiting_periods []
+  let signed_up_players =
+    try loop server conf.num_of_waiting_periods []
+    with error -> Unix.close server; raise error
+  in
+  Unix.close server;
+  signed_up_players
 ;;
