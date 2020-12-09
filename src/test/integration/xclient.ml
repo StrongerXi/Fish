536bd92efd21818da2af503d39b2e4d7328d5006
Ryan <ryan.guo99@gmail.com>
2020-12-09 11:19:38 -0500

    Refactor client testing harness and decrease name length

--- src/test/integration/xclient.ml
+++ src/test/integration/xclient.ml
@@ -22,22 +22,25 @@ let parse_config (args : string array) : config option =
     Option.return { num_clients; port; server_ip; search_depth = 2 }
 ;;
 
+let create_ith_player (conf : config) (i : int) : Player.t =
+  Player.create_AI_player
+    ~name:(Printf.sprintf "OCaml-AI-%d" i) ~age:i
+    Player.Strategy.Penguin_placer.create_scanning_strategy
+    (Player.Strategy.Turn_actor.create_minimax_strategy conf.search_depth)
+;;
+
+let create_ith_thread (conf : config) (i : int) : Thread.t =
+  let player = create_ith_player conf i in
+  Thread.create (fun () ->
+      Remote_player.interact_with_proxy player conf.server_ip conf.port)
+    ~on_uncaught_exn:`Print_to_stderr ()
+;;
+
 (* Start up a server for fish game at specified port *)
 let () =
   match parse_config @@ Sys.get_argv () with
   | None ->
     Printf.printf "Input: <number-of-clients> <port-number> <ip-address>";
   | Some(conf) ->
-    let my_ip = Unix.Inet_addr.localhost |> Unix.Inet_addr.to_string in
-    let threads = 
-      List.init conf.num_clients ~f:(fun i ->
-          let player = Player.create_AI_player
-              ~name:(Printf.sprintf "AI-%s-%d" my_ip i) ~age:i
-              Player.Strategy.Penguin_placer.create_scanning_strategy
-              (Player.Strategy.Turn_actor.create_minimax_strategy conf.search_depth)
-          in
-          Thread.create (fun () ->
-              Remote_player.interact_with_proxy player conf.server_ip conf.port)
-            ~on_uncaught_exn:`Print_to_stderr ())
-    in
-    List.iter ~f:Thread.join threads;
+    List.init conf.num_clients ~f:(create_ith_thread conf)
+    |> List.iter ~f:Thread.join;
