a940e3bb099854d55bb903ed98e659639aaf1385
Ryan <ryan.guo99@gmail.com>
2020-12-14 17:02:03 -0500

    [signup_manager] Refactor out logic for reading client name and add more documentation.

--- src/lib/remote/signup_manager.ml
+++ src/lib/remote/signup_manager.ml
@@ -49,6 +49,15 @@ let create_server (conf : config) (port : int) : Unix.File_descr.t =
 let handle_waiting_period (server : Unix.File_descr.t)
     (conf : config) (connected_players : Player.t list) : Player.t list =
   let timer = Timer.create conf.waiting_period_ms in
+  (* Return [None] on any communication failure or if [timer] expires *)
+  let read_name (ic : In_channel.t) : string option =
+    Option.bind ~f:Fn.id @@ (* don't differentiate timeout and io errors *)
+    Timeout.call_with_timeout_ms (fun () ->
+        try S.stream_from_channel ic |> Stream.next |> S.to_string
+        with _ -> None (* catch io exceptions *))
+      (Int.min (Timer.get_time_left_ms timer) conf.name_reply_timeout_ms)
+  in
+  (* Keep accepting new player until maximum # is reached or [timer] expires *)
   let rec loop (players : Player.t list) : Player.t list =
     if (List.length players) >= conf.max_num_of_players ||
        Timer.get_time_left_ms timer < 0
@@ -59,19 +68,15 @@ let handle_waiting_period (server : Unix.File_descr.t)
               (Timer.get_time_left_ms timer) with
       | None -> players (* this waiting period is over *)
       | Some(client_sock, _) -> loop @@ try_to_add_client players client_sock
-  (* Add [client_sock] as a proxy player to [players] if it sends a name in time *)
+  (* Add [client_sock] as a proxy player to [players] if it sends in a name
+   * before [timer] expires *)
   and try_to_add_client
       (players : Player.t list)
       (client_sock : Unix.File_descr.t) : Player.t list =
-    try
+    try (* catch io exceptions *)
       let ic = Unix.in_channel_of_descr client_sock in
       let oc = Unix.out_channel_of_descr client_sock in
-      let opt_name =
-        Option.bind ~f:Fn.id @@ (* don't differentiate timeout and io errors *)
-        Timeout.call_with_timeout_ms
-          (fun () -> S.stream_from_channel ic |> Stream.next |> S.to_string)
-          (Int.min (Timer.get_time_left_ms timer) conf.name_reply_timeout_ms) in
-      match opt_name with
+      match read_name ic with
       | None -> Unix.close client_sock; players (* properly release resources *)
       | Some(name) ->
         let age = List.length players in
